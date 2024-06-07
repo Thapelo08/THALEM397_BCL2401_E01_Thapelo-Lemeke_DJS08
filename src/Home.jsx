1c747f59b3fa3010577e7096134a6fef056579a9
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-07 14:08:08 +0200

    Created Home component with introductory content and link to Vans page.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
@@ -8,5 +8,5 @@ export default function Home() {
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans">Find your van</Link>
         </div>
-    );
+    )
 }
