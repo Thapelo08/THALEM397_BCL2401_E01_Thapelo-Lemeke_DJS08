a7bd16fdd8b4433e38076b2bb7d910d68862c086
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-07 14:29:13 +0200

    Updated Header component with navigation links, logo, and login/logout functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
 
 export default function Header() {
     const activeStyles = {
@@ -11,32 +12,32 @@ export default function Header() {
     function fakeLogOut() {
         localStorage.removeItem("loggedin")
     }
-    
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink 
+                <NavLink
                     to="/host"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/about"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/vans"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
-                        src="../assets/images/avatar-icon.png" 
+                    <img
+                        src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
