80791aeb1f02aacb81f6b9692f6c28350a6cf802
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-07 14:06:22 +0200

    Implemented login functionality with form submission and error handling.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { loginUser } from "./api";
-
+import React from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import { loginUser } from "../api"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const [status, setStatus] = React.useState("idle")
     const [error, setError] = React.useState(null)
-    
+
     const location = useLocation()
     const navigate = useNavigate()
-    
+
+    const from = location.state?.from || "/host";
+
     function handleSubmit(e) {
         e.preventDefault()
         setStatus("submitting")
@@ -18,7 +19,7 @@ export default function Login() {
             .then(data => {
                 setError(null)
                 localStorage.setItem("loggedin", true)
-                navigate("/host")
+                navigate(from, { replace: true })
             })
             .catch(err => {
                 setError(err)
@@ -40,14 +41,14 @@ export default function Login() {
         <div className="login-container">
             {
                 location.state?.message &&
-                <h3 className="login-error">{location.state.message}</h3>
+                    <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
             {
                 error?.message &&
-                <h3 className="login-error">{error.message}</h3>
+                    <h3 className="login-error">{error.message}</h3>
             }
-            
+
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -63,11 +64,11 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button 
+                <button
                     disabled={status === "submitting"}
                 >
-                    {status === "submitting" 
-                        ? "Logging in..." 
+                    {status === "submitting"
+                        ? "Logging in..."
                         : "Log in"
                     }
                 </button>
